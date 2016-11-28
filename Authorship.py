fbfdbbe7bcd516393237286519ea5ae0d3ddb193
d1alat3d <alexandrakroupa@gmail.com>
2016-11-27 20:50:11 -0800

    Made git ignore to bypass .idea directory and added my assigned functions to authorship

--- Authorship.py
+++ Authorship.py
@@ -1,7 +1,22 @@
-user = int(input('Enter a number between one and ten:'))
-    if user > 10:
-        print('This number is too large!')
-    elif user < 1:
-        print('This number is too small!')
-    else:
-        print(user*10)
+def split_on_separators(original, separators):  # alex k
+    ''' Return a list of non-empty, non-blank strings from the original string
+    determined by splitting the string on any of the separators.
+    separators is a string of single-character separators.'''
+
+    # To do: Complete this function's body to meet its specification.
+    # You are not required to keep the two lines below but you may find
+    # them helpful. (Hint)
+    result = [original]
+    return result
+
+
+def average_sentence_length(text):  # alex k
+    ''' Return the average number of words per sentence in text.
+    text is guaranteed to have at least one sentence.
+    Terminating punctuation defined as !?.
+    A sentence is defined as a non-empty string of non-terminating
+    punctuation surrounded by terminating punctuation
+    or beginning or end of file. '''
+
+    # To do: Replace this function's body to meet its specification.
+    return 1
